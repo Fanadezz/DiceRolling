01f6b6ad94da614434398c434ef1b237e6bf8497
Antony <vontonnie@gmail.com>
2020-11-09 20:59:02 +0300

    Add images

--- app/src/main/java/com/androidshowtime/dicerolling/MainActivity.kt
+++ app/src/main/java/com/androidshowtime/dicerolling/MainActivity.kt
@@ -1,20 +1,32 @@
 package com.androidshowtime.dicerolling
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var random: Random
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+random = Random(100)
 
-roll_button.text = "Let's Roll"
+
+        roll_button.text = "Let's Roll"
         roll_button.setOnClickListener {
 
-            Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+           rollDice()
         }
-        }
+    }
+
+    private fun rollDice() {
+
+        //textView.text = random.nextInt(1,7).toString()
+
+    }
+
 
 }
