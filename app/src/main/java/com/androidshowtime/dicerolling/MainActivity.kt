5fe9e89db9216359f64b3deb94d269c30648fe0c
Antony <vontonnie@gmail.com>
2020-11-09 21:25:47 +0300

    Roll Dice

--- app/src/main/java/com/androidshowtime/dicerolling/MainActivity.kt
+++ app/src/main/java/com/androidshowtime/dicerolling/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.androidshowtime.dicerolling
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlin.random.Random
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,7 +14,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-random = Random(100)
+random = Random(0)
 
 
         roll_button.text = "Let's Roll"
@@ -24,8 +26,25 @@ random = Random(100)
 
     private fun rollDice() {
 
-        //textView.text = random.nextInt(1,7).toString()
+       val randomInt = random.nextInt(1,7)
 
+        val diceDrawable = when(randomInt){
+
+
+
+            1-> R.drawable.dice_1
+            2-> R.drawable.dice_2
+            3-> R.drawable.dice_3
+            4-> R.drawable.dice_4
+            5-> R.drawable.dice_5
+
+
+
+
+            else ->R.drawable.dice_6
+        }
+
+        dice_imageView.setImageResource(diceDrawable)
     }
 
 
