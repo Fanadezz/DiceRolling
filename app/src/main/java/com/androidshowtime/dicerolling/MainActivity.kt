de839dfd047f6410a79c898af14a15af73f7bb2a
Antony <vontonnie@gmail.com>
2020-11-09 18:30:51 +0300

    Set Button's OnclickListener

--- app/src/main/java/com/androidshowtime/dicerolling/MainActivity.kt
+++ app/src/main/java/com/androidshowtime/dicerolling/MainActivity.kt
@@ -2,12 +2,19 @@ package com.androidshowtime.dicerolling
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-roll
-    }
+roll_button.text = "Let's Roll"
+        roll_button.setOnClickListener {
+
+            Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+        }
+        }
+
 }
